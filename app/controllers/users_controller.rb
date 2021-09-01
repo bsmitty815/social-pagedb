1324f55f1a0369b3fcdce7b85abf165c12a8d7a0
Brian Smith <brian.bubbam@gmail.com>
2021-09-01 11:59:57 -0500

    set up user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,38 @@
 class UsersController < ApplicationController
-    has_secure_password
 
-    has_one :profile, dependent: :destroy
 
-    validates :username, presence: true, uniqueness: true
+    #create user
+    def create
+        user = User.new(user_params)
+        #byebug
+        if user.save
 
+            session[:user_ud] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+
+    end
+
+    #show user
+    def show
+        #byebug
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else
+            render json: { error: "Unauthorized user" }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_params
+        #if you use params.require you need to send it as a key of user from the front end / user: {}
+        #params.require(:user).permit(:username, :password, :password_confirmation, :image)
+        params.permit(:username, :password, :password_confirmation, :image)
+    end
+
+    
 end
