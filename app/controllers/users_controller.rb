0804129f40a692e46d07a958227c3bb9c376f863
Brian Smith <brian.bubbam@gmail.com>
2021-09-01 11:56:21 -0500

    setup belings to has one

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
     has_secure_password
-    
+
+    has_one :profile, dependent: :destroy
+
+    validates :username, presence: true, uniqueness: true
+
 end
